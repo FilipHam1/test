60c38651e7405463e56e4ac3af5ad44a56c154b1
FilipHam1 <filip.hamrik@educanet.cz>
2026-01-13 11:31:01 +0100

    Add Person.php from previous task

---
+++ Person.php
@@ -0,0 +1,23 @@
+<?php
+
+class Person
+{
+    protected string $name;
+    protected int $age;
+
+    public function __construct(string $name, int $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getInfo(): string
+    {
+        return "{$this->name}, {$this->age} let";
+    }
+}
