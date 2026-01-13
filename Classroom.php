4ad8471e1b98f0928b6911f5b8a07ff858dc6c44
FilipHam1 <filip.hamrik@educanet.cz>
2026-01-13 11:19:56 +0100

    construct

--- Classroom.php
+++ Classroom.php
@@ -1,4 +1,5 @@
 <?php
+require_once "Student.php";
 
 class Classroom
 {
@@ -6,20 +7,18 @@ class Classroom
     public int $capacity;
     public array $students;
 
-    // konstruktor
     public function __construct(string $label, int $capacity)
     {
         $this->label = $label;
         $this->capacity = $capacity;
-        $this->students = []; 
+        $this->students = [];
     }
 
-    // metoda navíc – naplnění pole objektů
     public function enroll(Student $student): void
     {
         if (count($this->students) < $this->capacity) {
             $this->students[] = $student;
-            echo "{$student->name} byl zapsán do třídy {$this->label}.<br>";
+            echo $student->getName() . " byl zapsán do třídy {$this->label}.<br>";
         } else {
             echo "Třída {$this->label} je plná.<br>";
         }
